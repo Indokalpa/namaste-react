17dea88a52603d318a65a9557121e2237d4e9ad4
indokalpa <indokalpasaikia1@gmail.com>
2025-03-05 12:39:24 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,33 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/**
- * <div id="parent">
- *    <div id="child1">
- *        <h1>I am an h1 tag</h1>
- *        <h2>I am an h2 tag</h2>
- *    </div>
- *    <div id="child2">
- *       <h1>I am an h1 tag</h1>
- *       <h2>I am an h2 tag</h2>
- *    </div>
- * </div> 
- */
-// React can also be written without JSX, but it is quite inconvinient.
-const parent = React.createElement("div",{id:"parent"},
-    [React.createElement("div",{id:"child1"},
-    [React.createElement("h1",{},"I am an h1 tag"),
-     React.createElement("h2",{},"I am an h2 tag")
-    ]),
-    React.createElement("div",{id:"child2"},
-    [React.createElement("h1",{},"I am an h1 tag"),
-    React.createElement("h2",{},"I am an h2 tag")
-    ]),
-]
-);
+// React.createElement() =>JS object => HTML element(on render)
+ const headingElement = React.createElement(
+    "h1",
+    {id: "heading"},
+    " Namaste React 🚀"
+    )
 
+// JSX is not HTML in JS, just their syntax is same.
+// JSX is not understable by JS engine. 
+//JSX => React.createElement(Babel in parcel) => JS object => HTML Element(on render). 
 
-// heading is react element, which is JS object.
-const heading = React.createElement("h1",{id: "heading"},"Hello World!   from React!");
+// React element
+ const jsxHeading = <h1 className="head"> Namaste React using JSX !🚀</h1>;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+ // use camel-casing to give attribute in JSX.we use 'className' not 'class'
+ // For multiple line JSX, use parenthesis.
+  
+ // React component 1> class Based component 2> Functional component
+ const HeadingComponent = () => {
+    return <h1 className="heading">Heading from Functional component 1</h1>;
+ }
+ // syntax without 'return' and ' { } '
 
-root.render(parent);// takes JS object,convert to 'h1' HTML element and put it up on the DOM
+ const number = 1000;
+ const HeadingComponent2 = () => (
+    <div id="container">
+        {
+         // can write any javascript inside {}, so we can put react element, component also. 
+         HeadingComponent()
+        }
+        <HeadingComponent />
+        <HeadingComponent></HeadingComponent>
+        <h1 className="heading">Heading from Functional component 2</h1>
+    </div>
+ ) 
+    
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ 
+ // render React Element
+ // root.render(jsxHeading);
+
+ // render React Component
+ root.render(<HeadingComponent2 />);
